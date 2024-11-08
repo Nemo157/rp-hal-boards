0012a89e321b62e92aa901684b55fb52da32478f
Wim Looman <git@nemo157.com>
2024-11-08 17:06:24 +0100

    Add usb example

---
+++ boards/waveshare-rp2040-epaper-73/examples/waveshare_rp2040_epaper_73_usb.rs
@@ -0,0 +1,127 @@
+#![no_std]
+#![no_main]
+
+use embedded_hal::{delay::DelayNs, digital::OutputPin};
+use panic_halt as _;
+use usbd_serial::SerialPort;
+use usb_device::{device::{UsbDeviceBuilder, StringDescriptors, UsbVidPid}, bus::UsbBusAllocator};
+use heapless::String;
+use core::fmt::Write;
+
+use waveshare_rp2040_epaper_73::{
+    entry,
+    hal::{clocks::init_clocks_and_plls, pac, timer::Timer, watchdog::Watchdog, Sio, usb::UsbBus},
+    LedActivity, Pins, XOSC_CRYSTAL_FREQ, LedPower,
+};
+
+#[entry]
+fn main() -> ! {
+    let mut pac = pac::Peripherals::take().unwrap();
+    let mut watchdog = Watchdog::new(pac.WATCHDOG);
+    let sio = Sio::new(pac.SIO);
+
+    let clocks = init_clocks_and_plls(
+        XOSC_CRYSTAL_FREQ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
+    let mut timer = Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
+
+    let pins = Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    let mut led_power: LedPower = pins.led_power.reconfigure();
+    let mut led_activity: LedActivity = pins.led_activity.reconfigure();
+
+    // Set up the USB driver
+    let usb_bus = UsbBusAllocator::new(UsbBus::new(
+        pac.USBCTRL_REGS,
+        pac.USBCTRL_DPRAM,
+        clocks.usb_clock,
+        true,
+        &mut pac.RESETS,
+    ));
+
+    let mut serial = SerialPort::new(&usb_bus);
+
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+        .strings(&[StringDescriptors::default()
+            .manufacturer("Fake company")
+            .product("Serial port")
+            .serial_number("TEST")])
+        .unwrap()
+        .device_class(2) // from: https://www.usb.org/defined-class-codes
+        .build();
+
+    led_power.set_high().unwrap();
+
+    let mut said_hello = false;
+    loop {
+        // A welcome message at the beginning
+        if !said_hello && timer.get_counter().ticks() >= 2_000_000 {
+            led_activity.set_high().unwrap();
+            timer.delay_ms(500);
+
+            said_hello = true;
+            let _ = serial.write(b"Hello, World!\n");
+
+            timer.delay_ms(500);
+
+            let time = timer.get_counter().ticks();
+            let mut text: String<64> = String::new();
+            writeln!(&mut text, "Current timer ticks: {}", time).unwrap();
+
+            // This only works reliably because the number of bytes written to
+            // the serial port is smaller than the buffers available to the USB
+            // peripheral. In general, the return value should be handled, so that
+            // bytes not transferred yet don't get lost.
+            let _ = serial.write(text.as_bytes());
+
+            timer.delay_ms(500);
+            led_activity.set_low().unwrap();
+        }
+
+        // Check for new data
+        if usb_dev.poll(&mut [&mut serial]) {
+            let mut buf = [0u8; 64];
+            match serial.read(&mut buf) {
+                Err(_e) => {
+                    // Do nothing
+                }
+                Ok(0) => {
+                    // Do nothing
+                }
+                Ok(count) => {
+                    led_activity.set_high().unwrap();
+                    // Convert to upper case
+                    buf.iter_mut().take(count).for_each(|b| {
+                        b.make_ascii_uppercase();
+                    });
+                    // Send back to the host
+                    let mut wr_ptr = &buf[..count];
+                    while !wr_ptr.is_empty() {
+                        match serial.write(wr_ptr) {
+                            Ok(len) => wr_ptr = &wr_ptr[len..],
+                            // On error, just drop unwritten data.
+                            // One possible error is Err(WouldBlock), meaning the USB
+                            // write buffer is full.
+                            Err(_) => break,
+                        };
+                    }
+                    led_activity.set_low().unwrap();
+                }
+            }
+        }
+    }
+}
